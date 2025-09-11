44630a5a53efbdd75191231b5342cb3048f9eef2
Esaius2058 <wabwirewild@gmail.com>
2025-09-11 19:04:25 +0300

    refactor(OOP): switched from function based to class-based
    feat: added function list_files to list all the files uploaded by a user
    
    feat: added function update_upload_status to show the status of an upload

--- backend/src/services/storage.py
+++ backend/src/services/storage.py
@@ -1,39 +1,91 @@
 import supabase
 import logging
+
+from jeepney.low_level import Boolean
+
+from backend.src.db.models import Uploads
 from backend.src.services.supabase_client import supabase_client
 
-logger = logging.getLogger("Storage Service")
-bucket_name = "uploads"
+class StorageService:
+    def __init__(self, client: supabase.Client, bucket, db_session):
+        self.client = client
+        self.bucket = bucket
+        self.db = db_session
+        self.logger = logging.getLogger("Storage Service")
 
-def upload_file(file_path: str, file_name: str):
-    try:
-        with open(file_path, "rb") as f:
-            res = supabase.storage.from_(bucket_name).upload(file_name, f)
-        return res
-    except Exception as e:
-        logger.info(f"Error uploading file: {e}")
-        return None
+    def upload_file(self, file_obj, file_name: str, user_id):
+        try:
+            self.client.storage.from_(self.bucket).upload(file_name, file_obj)
+            public_url = self.get_public_url(file_name)
 
-def download_file(file_name: str, file_dest: str):
-    try:
-        res = supabase.storage.from_(bucket_name).download(file_name)
-        with open(file_dest, "wb") as f:
-            f.write(res)
-        return file_dest
-    except Exception as e:
-        logger.info(f"Error downloading file: {e}")
-        return None
+            new_upload = Uploads(
+                filename=file_name,
+                storage_ulr=public_url,
+                user_id=user_id
+            )
+            self.db.add(new_upload)
+            self.commit()
+            self.refresh(new_upload)
 
-def delete_file(file_name: str):
-    try:
-        return supabase.storage.from_(bucket_name).remove([file_name])
-    except Exception as e:
-        logger.info(f"Error downloading file: {e}")
-        return None
+            return new_upload
+        except Exception as e:
+            self.logger.info(f"Error uploading file: {e}")
+            self.db.rollback()
+            return None
 
-def get_public_url(file_name: str):
-    try:
-        return supabase.storage.from_(bucket_name).get_public_url([file_name])
-    except Exception as e:
-        logger.info(f"Error downloading file: {e}")
-        return None
+    def download_file(self, file_name: str, file_dest: str):
+        try:
+            res = supabase.storage.from_(bucket_name).download(file_name)
+            with open(file_dest, "wb") as f:
+                f.write(res)
+            return file_dest
+        except Exception as e:
+            self.logger.info(f"Error downloading file: {e}")
+            return None
+
+    def delete_file(self, file_name: str):
+        try:
+            supabase.storage.from_(bucket_name).remove([file_name])
+
+            upload = self.db.query(Uploads).filter_by(filename=file_name).first()
+            if upload:
+                self.db.delete(upload)
+                self.db.commit()
+            return f"{file_name} deleted successfully"
+        except Exception as e:
+            self.logger.info(f"Error deleting file: {e}")
+            self.db.rollback()
+            return None
+
+    def get_public_url(self, file_name: str):
+        try:
+            return supabase.storage.from_(bucket_name).get_public_url([file_name])
+        except Exception as e:
+            self.logger.info(f"Error getting public url for '{file_name}': {e}")
+            return None
+
+    def list_files(self, user_id=None):
+        try:
+            query = self.db.query(Uploads)
+            if user_id:
+                query = query.filter_by(user_id=user_id)
+            return query.all()
+        except Exception as e:
+            self.logger.info(f"Error listing uploaded files: {e}")
+            self.db.rollback()
+            return None
+
+    def update_upload_status(self, upload_id, status) -> bool:
+        try:
+            upload = self.db.query(Uploads).filter_by(id=upload_id).first()
+            if not upload:
+                return False
+
+            upload.status = status
+            self.db.commit()
+            self.db.refresh(upload)
+            return True
+        except Exception as e:
+            self.logger.info(f"Error updating upload status: {e}")
+            self.db.rollback()
+            return False
