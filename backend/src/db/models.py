ce1de25e5d34b5c52ac53624ade4fc911274dff1
Esaius2058 <wabwirewild@gmail.com>
2025-09-14 21:37:09 +0300

    feat: model ExamContent to keep track of the content extractedd from the uploaded files

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -98,6 +98,13 @@ class Uploads(Base):
     created_at = Column(TIMESTAMP, server_default=func.now())
     updated_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
 
+class ExamContent(Base):
+    __tablename__ = "exam_content"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
+    upload_id = Column(UUID(as_uuid=True), ForeignKey("uploads.id"))
+    text = Column(Text, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
