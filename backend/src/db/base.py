618661b40da080b7358850a94bc90affc00dfc64
Esaius2058 <wabwirewild@gmail.com>
2025-09-21 21:19:45 +0300

    feat: create a global databse session
    feat: create a function to call the db instance

--- backend/src/db/base.py
+++ backend/src/db/base.py
@@ -1,11 +1,20 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 db_url = os.getenv("DATABASE_URL")
 
 engine = create_engine(db_url, echo=True)
 
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
